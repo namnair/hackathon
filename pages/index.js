7db40fa247cc4c89916a3390f01f22fe93eb2e9b
namnair <93312156+namnair@users.noreply.github.com>
2023-06-24 15:31:08 +0530

    Co-authored-by: Nibil Mathew <nibilmathew@users.noreply.github.com> Co-authored-by: Nk0x1 <Nk0x1@users.noreply.github.com> Co-authored-by: EzekVP <EzekVP@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import Image from "next/image";
+import data from '../data.json'
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={``}>
-   
+  return <main className="">
+    <div className="bg-gray-100 h-80">
+      <input type="text" placeholder="search.." className="border border-gray-300 rounded-lg p-2  ">
+      </input>
+    </div>
+   <div className="flex flex-col gap-4 justify-center items-center h-screen m-5">
+   {data.map((item) => (
+          <div key={item.id}>
+            <button className="bg-blue-800 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">
+            <h1 className="m-1">{item.name}</h1>
+            <div className="flex flex-row p-2">
+            <p className="p-5">{item.desc}</p>
+            <div className="h-20 w-20"><img src={item.img}/></div>
+            </div>
+            </button>
+          </div>
+        ))}
+   </div>
   </main>;
 }
