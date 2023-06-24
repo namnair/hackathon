8ba9f30a56e1770933fa83b6077eca5aa9bc113c
namnair <93312156+namnair@users.noreply.github.com>
2023-06-24 21:22:49 +0530

    final

--- pages/[service].js
+++ pages/[service].js
@@ -34,17 +34,17 @@ export default function Service() {
   }, [isVisible]);
 
   if (loading) {
-    return <div>Loading...</div>; // Display loading state while fetching data
+    return <div></div>; // Display loading state while fetching data
   }
 
   if (!serviceData) {
-    return <div>Service not found</div>;
+    return <div></div>;
   }
 
   return (
     <div className="">
       {isVisible && <Confetti />}
-      <div className="h-48 text-lg bg-black text-left text-white items-center justify-center flex">
+      <div className="h-48 text-lg bg-blue-500 text-left text-white items-center justify-center flex">
         <h2 className="text-3xl font-bold pt-5 pl-3">{serviceData.name}</h2>
       </div>
       <div className=" flex flex-col justify-center items-center">
@@ -52,12 +52,12 @@ export default function Service() {
         <ul className="grid grid-cols-2 gap-4 max-w-fit">
           {serviceData.categories.map((category) => (
             <li className="p-2" key={category.name}>
-              <div className=" h-64 w-64 text-lg bg-gray-200 m-4 border-2 border-gray-300 rounded-lg p-4 max-w-md mx-auto relative">
-                <h3 className="text-2xl text-center mb-4">{category.name}</h3>
-                <p className=" text-justify mb-4">{category.desc}</p>
-                <h3 className="">{category.price}</h3>
+              <div className=" h-64 w-64 text-lg bg-slate-100 shadow-md m-4 rounded-lg p-4 max-w-md mx-auto relative">
+                <h3 className="text-2xl font-semibold text-center mb-4">{category.name}</h3>
+                <p className=" justify-center text-black text-opacity-70 mb-4">{category.desc}</p>
+                <h3 className="font-bold text-right">₹{category.price}</h3>
                 <button
-                  className="absolute bottom-4 right-4 px-4 py-2 bg-blue-500 text-white rounded "
+                  className="absolute bottom-4 right-4 px-4 py-2 bg-green-400 hover:bg-green-300 text-white rounded-lg "
                   onClick={() => {
                     setIsVisible(true);
                   }}
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,76 @@
-import Image from "next/image";
-import data from "../data.json";
+import { useState } from "react";
 import { Inter } from "next/font/google";
+import data from "../data.json";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    const results = data.filter((item) => {
+      return item.name.toLowerCase().includes(value.toLowerCase());
+    });
+
+    setSearchResults(results);
+  };
+
   return (
-    <main className="">
-      <div class="grid place-items-center bg-green-300 p-28 rounded-b-3xl">
-        <h2 className="text-5xl font-bold mb-7">How can we help you?</h2>
-        <input
-          type="text"
-          placeholder="Search"
-          className="border border-gray-300 rounded-lg p-2 w-1/2"
-          autoFocus
-        ></input>
+    <main className="  flex space-x-4">
+      <div className=" w-80 h-96  rounded-bl-full rounded-br-full bg-green-300 ">
+        <h2 className="text-7xl mt-7 ml-7 font-bold ">How can we help you?</h2>
+        <div className="relative p-10">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearch}
+            className="w-56 px-4 py-2 border shadow hover:shadow-lg border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          />
+          {searchTerm && (
+            <ul className="absolute left-0 right-0 mt-2 bg-white rounded-lg shadow-lg">
+              {searchResults.map((item) => (
+                <li
+                  key={item.id}
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                >
+                   <a href={`/${item.name.toLowerCase()}`}>{item.name}</a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-      <div className="flex flex-col gap-4 justify-center items-center my-10">
-        {data.map((item) => (
-          <div key={item.id}>
-            <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">
-              <a href={"/" + item.name.toLowerCase()}>
-                <div className="flex flex-row">
-                  <div className="flex flex-col p-2 px-5">
-                    <h1 className="m-1 text-3xl">{item.name}</h1>
-                    <p className="m-3">{item.desc}</p>
-                  </div>
-                  <div className="w-40 max-w-fit max-h-fit">
-                    <img src={item.img} className="rounded-lg" />
-                  </div>
-                </div>
-              </a>
-            </button>
+      <div className="flex flex-col gap-4 mt-5">
+  {data.map((item) => (
+    <div className="ml-20" key={item.id}>
+      <button className="bg-blue-400 hover:bg-blue-500 text-white ml-20 font-bold py-2 px-9 rounded-full">
+        <a href={`/${item.name.toLowerCase()}`}>
+          <div className="flex flex-row">
+            <div className="flex flex-col p-2 px-5 w-72"> {/* Adjust the width as needed */}
+              <h1 className="m-1 text-3xl overflow-hidden">{item.name}</h1> {/* Add overflow-hidden to truncate long text */}
+              <p className="m-3 overflow-hidden">{item.desc}</p> {/* Add overflow-hidden to truncate long text */}
+            </div>
+            <div className="w-40 h-40">
+              <img
+                src={item.img}
+                alt={item.name}
+                className="rounded-full object-cover"
+                width={160}
+                height={160}
+              />
+            </div>
           </div>
-        ))}
-      </div>
+        </a>
+      </button>
+    </div>
+  ))}
+</div>
+
     </main>
   );
 }
